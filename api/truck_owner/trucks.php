2c8bf7a6cf8b3fe685d22558fe03f097cd814a22
unknown <rohit1995sgh@gmail.com>
2020-07-30 18:18:41 +0530

    View and Delete Trucks API complete

--- api/truck_owner/trucks.php
+++ api/truck_owner/trucks.php
@@ -4,7 +4,68 @@
 
     header('Content-Type: application/json');
 
-    if(isset($_POST['trk_owner']) && isset($_POST['trk_cat']) && isset($_POST['trk_num']) && isset($_POST['trk_load']) && isset($_POST['trk_dr_name']) && 
+    if(isset($_POST['truck_owner_id']))
+    {
+        $sql = "select * from trucks where trk_owner = '".$_POST['truck_owner_id']."'";
+        $result = mysqli_query($link, $sql) or die("Error in Selecting " . mysqli_error($link));
+
+        //create an array
+        $emparray = array();
+        while($row = mysqli_fetch_assoc($result))
+        {
+            $emparray[] = $row;
+        }
+        
+        echo json_encode($emparray, JSON_PRETTY_PRINT);
+
+        http_response_code(200);
+        
+        mysqli_close($link);
+    }
+    // elseif(isset($_POST['cu_id']) && isset($_POST['order_id']))
+    // {
+    //     $d_sql = "update orders set order_status = '2' where order_id = '".$_POST['order_id']."'";
+    //     $d_run = mysqli_query($link, $d_sql);
+    //     if($d_run)
+    //     {
+    //         $responseData = ['success' => '1', 'message' => 'Order cancelled'];
+    //         echo json_encode($responseData, JSON_PRETTY_PRINT);
+    //     }
+    //     else
+    //     {
+    //         $responseData = ['success' => '0', 'message' => 'Order cancel failed'];
+    //         echo json_encode($responseData, JSON_PRETTY_PRINT);
+    //     }
+    // }
+    elseif(isset($_POST['del_truck_id']))
+    {
+        $sql = "select * from trucks where trk_id = '".$_POST['del_truck_id']."'";
+        $result = mysqli_query($link, $sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+        $dirname = "../../assets/documents/truck_owners/truck_owners_id_".$row['trk_owner']."/".$row['trk_num'];
+
+        array_map('unlink', glob("$dirname/*.*"));
+        rmdir("$dirname");
+
+        $d_sql = "delete from trucks where trk_id = '".$_POST['del_truck_id']."'";
+        $d_run = mysqli_query($link, $d_sql);
+        if($d_run)
+        {
+            $responseData = ['success' => '1', 'message' => 'Truck removed'];
+            echo json_encode($responseData, JSON_PRETTY_PRINT);
+
+            http_response_code(200);
+        }
+        else
+        {
+            $responseData = ['success' => '0', 'message' => 'Failed to remove'];
+            echo json_encode($responseData, JSON_PRETTY_PRINT);
+
+            http_response_code(400);
+        }
+    }
+    elseif(isset($_POST['trk_owner']) && isset($_POST['trk_cat']) && isset($_POST['trk_num']) && isset($_POST['trk_load']) && isset($_POST['trk_dr_name']) && 
         isset($_POST['trk_dr_phone_code']) && isset($_POST['trk_dr_phone']))
     {
         $truck_owner = $_POST['trk_owner'];
@@ -242,6 +303,13 @@
                 http_response_code(206);
             }
         }
+    }
+    else
+    {
+        $responseData = ['success' => '0', 'message' => 'Something went wrong'];
+        echo json_encode($responseData, JSON_PRETTY_PRINT);
+
+        http_response_code(400);
     }
 
 ?>
