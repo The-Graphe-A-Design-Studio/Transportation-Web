426603398e22f505625beb7857fc6739c18111ec
unknown <rohit1995sgh@gmail.com>
2020-08-03 12:31:00 +0530

    truck owner profile basic details updated

--- truck_owner_profile.php
+++ truck_owner_profile.php
@@ -1,6 +1,11 @@
 <?php
     include('session.php');
     include('layout.php');
+    $owner = $_GET['owner_id'];
+
+    $sql = "select * from truck_owners where to_id = '$owner'";
+    $g_sql = mysqli_query($link, $sql);
+    $row = mysqli_fetch_array($g_sql, MYSQLI_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -31,58 +36,52 @@
                 </div>
 
                 <div class="section-body">
-                    <h2 class="section-title">Hi, Ujang!</h2>
-                    <p class="section-lead">
-                        Change information about yourself on this page.
-                    </p>
                     <div class="row mt-sm-4">
-                        <div class="col-12 col-md-12 col-lg-5">
+                        <div class="col-12 col-md-12 col-lg-6">
                             <div class="card profile-widget">
                                 <div class="profile-widget-header">
-                                <img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle profile-widget-picture">
+                                <img alt="image" src="assets/img/avatar/avatar-2.png" class="rounded-circle profile-widget-picture">
                                 <div class="profile-widget-items">
                                     <div class="profile-widget-item">
-                                        <div class="profile-widget-item-label">Posts</div>
-                                        <div class="profile-widget-item-value">187</div>
+                                        <div class="profile-widget-item-label">Email</div>
+                                        <div class="profile-widget-item-value"><a href="mailto:<?php echo $row['to_email']; ?>"><?php echo $row['to_email']; ?></a></div>
                                     </div>
                                     <div class="profile-widget-item">
-                                        <div class="profile-widget-item-label">Followers</div>
-                                        <div class="profile-widget-item-value">6,8K</div>
+                                        <div class="profile-widget-item-label">Phone</div>
+                                        <div class="profile-widget-item-value">+<?php echo $row['to_phone_code']." ".$row['to_phone']; ?></div>
                                     </div>
                                     <div class="profile-widget-item">
-                                        <div class="profile-widget-item-label">Following</div>
-                                        <div class="profile-widget-item-value">2,1K</div>
+                                        <div class="profile-widget-item-label">City</div>
+                                        <div class="profile-widget-item-value"><?php echo $row['to_city']; ?></div>
                                     </div>
                                 </div>
                                 </div>
                                 <div class="profile-widget-description">
                                 <div class="profile-widget-name">
-                                    Ujang Maman 
-                                    <div class="text-muted d-inline font-weight-normal">
-                                        <div class="slash"></div>
-                                        Web Developer
-                                    </div>
+                                    <h5><?php echo $row['to_name']; ?></h5>
                                 </div>
-                                Ujang maman is a superhero name in <b>Indonesia</b>, especially in my family. He is not a fictional character but an original hero in my family, a hero for his children and for his wife. So, I use the name as a user in this template. Not a tribute, I'm just bored with <b>'John Doe'</b>.
+                                    <b>Address : </b><?php echo $row['to_address']; ?>
+                                    <br>
+                                    <b>Operating Routes : </b><?php echo $row['to_routes']; ?>
+                                    <br>
+                                    <b>State Permits : </b><?php echo $row['to_permits']; ?>
+                                    <br>
+                                    <b>Bank Account Number : </b><?php echo $row['to_bank']; ?>
+                                    &nbsp;&nbsp;&nbsp;
+                                    <b>IFSC Code : </b><?php echo $row['to_ifsc']; ?>
+                                    <br>
+                                    <b>PAN Number : </b><?php echo $row['to_pan']; ?>
                                 </div>
                                 <div class="card-footer text-center">
-                                <div class="font-weight-bold mb-2">Follow Ujang On</div>
-                                <a href="#" class="btn btn-social-icon btn-facebook mr-1">
-                                <i class="fab fa-facebook-f"></i>
-                                </a>
-                                <a href="#" class="btn btn-social-icon btn-twitter mr-1">
-                                <i class="fab fa-twitter"></i>
-                                </a>
-                                <a href="#" class="btn btn-social-icon btn-github mr-1">
-                                <i class="fab fa-github"></i>
-                                </a>
-                                <a href="#" class="btn btn-social-icon btn-instagram">
-                                <i class="fab fa-instagram"></i>
-                                </a>
+                                    <div class="font-weight-bold mb-2">
+                                        <b>Registered On :</b> <?php $date = date_create($row['to_registered']);
+                                            $date = date_format($date, "d M, Y");
+                                            echo $date; ?>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-12 col-md-12 col-lg-7">
+                        <div class="col-12 col-md-12 col-lg-6">
                             <div class="card">
                                 <form method="post" class="needs-validation" novalidate="">
                                 <div class="card-header">
