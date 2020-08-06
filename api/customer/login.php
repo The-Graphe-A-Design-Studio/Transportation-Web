f5b01ef263209496cb8df0ed94b2d9b57dcf1d89
unknown <rohit1995sgh@gmail.com>
2020-08-06 15:48:39 +0530

    customer login api complete

--- api/customer/login.php
+++ api/customer/login.php
@@ -4,20 +4,33 @@
 
     header('Content-Type: application/json');
 
-    if(isset($_POST['phone_code']) && isset($_POST['phone']) && isset($_POST['password']))
+    if(isset($_POST['cu_type']) && isset($_POST['phone_code']) && isset($_POST['phone']) && isset($_POST['password']))
     {
         $enc_password = md5($_POST['password']);
          
-        $login_sql = "SELECT * FROM customers WHERE cu_phone_code = '".$_POST['phone_code']."' and cu_phone = '".$_POST['phone']."' and cu_password = '$enc_password' and to_verified = '1'";
+        $login_sql = "SELECT * FROM customers WHERE cu_phone_code = '".$_POST['phone_code']."' and cu_phone = '".$_POST['phone']."' and cu_password = '$enc_password' and 
+                        cu_type = '".$_POST['cu_type']."' and cu_verified = '1'";
         $result = mysqli_query($link, $login_sql);
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
         $count = mysqli_num_rows($result);
         
-        if($count == 1)
+        if($count == 1 && $row['cu_type'] == 1)
         {
-            $responseData = ['success' => '1', 'id' => $row['to_id'], 'name' => $row['to_name'], 'email' => $row['to_email'], 'phone_con_code' => $row['to_phone_code'],
-            'phone' => $row['to_phone'], 'city' => $row['to_city'], 'address' => $row['to_address'], 'operating routes' => $row['to_routes'], 'permit states' => $row['to_permits'],
-            'pan' => $row['to_pan'], 'bank' => $row['to_bank'], 'ifsc' => $row['to_ifsc'], 'registered on' => $row['to_registered']];
+            $responseData = ['success' => '1', 'id' => $row['cu_id'], 'name' => $row['cu_name'], 'email' => $row['cu_email'], 'phone_con_code' => $row['cu_phone_code'],
+            'phone' => $row['cu_phone'], 'city' => $row['cu_city'], 'address' => $row['cu_address'], 'pin code' => $row['cu_pin_code'], 'pan number' => $row['cu_pan_num'],
+            'registered on' => $row['cu_registered']];
+            echo json_encode($responseData, JSON_PRETTY_PRINT);
+
+            http_response_code(200);
+            
+            mysqli_close($link);
+        }
+        elseif($count == 1 && $row['cu_type'] == 2)
+        {
+            $responseData = ['success' => '1', 'id' => $row['cu_id'], 'name' => $row['cu_name'], 'email' => $row['cu_email'], 'phone_con_code' => $row['cu_phone_code'],
+            'phone' => $row['cu_phone'], 'city' => $row['cu_city'], 'address' => $row['cu_address'], 'pin code' => $row['cu_pin_code'], 'company name' => $row['cu_co_name'], 
+            'company type' => $row['cu_co_type'], 'company service tax' => $row['cu_co_service_tax'], 'compnay pan number' => $row['cu_co_pan_num'], 
+            'company website' => $row['cu_co_website'], 'registered on' => $row['cu_registered']];
             echo json_encode($responseData, JSON_PRETTY_PRINT);
 
             http_response_code(200);
