8e69ba075eb1a5be88fb8cf855697db3d95e935b
unknown <rohit1995sgh@gmail.com>
2020-08-18 14:03:52 +0530

    customer create order api

---
+++ api/customer/create_order.php
@@ -0,0 +1,11 @@
+<?php
+    include('../../dbcon.php');
+
+    header('Content-Type: application/json');
+
+    if(isset($_POST['source']) && isset($_POST['destination']) && isset($_POST['date']) && isset($_POST['material']) && isset($_POST['truck_type']) && 
+    isset($_POST['load']) && isset($_POST['quote']))
+    {
+        
+    }
+?>
