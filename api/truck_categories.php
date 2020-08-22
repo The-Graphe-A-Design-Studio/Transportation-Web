e5787942bb69933e5875259c1a995bcaf8eefcc0
unknown <rohit1995sgh@gmail.com>
2020-08-22 15:08:19 +0530

    truck categories api

---
+++ api/truck_categories.php
@@ -0,0 +1,20 @@
+<?php
+    //open connection to mysql db
+    include('../dbcon.php');
+
+    //fetch table rows from mysql db
+    $sql = "select * from truck_cat";
+    $result = mysqli_query($link, $sql) or die("Error in Selecting " . mysqli_error($link));
+
+    //create an array
+    $emparray = array();
+    while($row =mysqli_fetch_assoc($result))
+    {
+        $emparray[] = $row;
+    }
+    header('Content-Type: application/json');
+    echo json_encode($emparray, JSON_PRETTY_PRINT);
+
+    //close the db connection
+    mysqli_close($link);
+?>
