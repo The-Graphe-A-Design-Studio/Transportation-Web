6fa5605e25f115d3d0e15a0e83e542cfd43aa046
unknown <rohit1995sgh@gmail.com>
2020-08-24 15:17:50 +0530

    material types api completed

---
+++ api/material_types.php
@@ -0,0 +1,21 @@
+<?php
+    //open connection to mysql db
+    include('../dbcon.php');
+
+    //fetch table rows from mysql db
+    $sql = "select * from material_types";
+    $result = mysqli_query($link, $sql) or die("Error in Selecting " . mysqli_error($link));
+
+    //create an array
+    $emparray = array();
+    while($row =mysqli_fetch_assoc($result))
+    {
+        $emparray[] = $row;
+    }
+    header('Content-Type: application/json');
+    echo json_encode($emparray, JSON_PRETTY_PRINT);
+    http_response_code(200);
+
+    //close the db connection
+    mysqli_close($link);
+?>
