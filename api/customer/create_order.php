0588c8cb91e3cab449643a5be14f4f73d4a0dfde
unknown <rohit1995sgh@gmail.com>
2020-08-27 14:14:56 +0530

    customer create order api complete

--- api/customer/create_order.php
+++ api/customer/create_order.php
@@ -3,9 +3,9 @@
 
     header('Content-Type: application/json');
 
-    if(isset($_POST['source']) && isset($_POST['destination']) && isset($_POST['material']) && isset($_POST['price_unit']) && isset($_POST['quantity']) && 
-        isset($_POST['truck_preference']) && isset($_POST['truck_types']) && isset($_POST['expected_price']) && isset($_POST['payment_mode']) && isset($_POST['advance_pay']) && isset($_POST['expire_date_time'])
-        && isset($_POST['contact_person_name']) && isset($_POST['contact_person_phone']))
+    if(isset($_POST['cust_id']) && isset($_POST['source']) && isset($_POST['destination']) && isset($_POST['material']) && isset($_POST['price_unit']) && isset($_POST['quantity']) && 
+        isset($_POST['truck_preference']) && isset($_POST['truck_types']) && isset($_POST['expected_price']) && isset($_POST['payment_mode']) && isset($_POST['advance_pay']) && 
+        isset($_POST['expire_date_time']) && isset($_POST['contact_person_name']) && isset($_POST['contact_person_phone']))
     {
         date_default_timezone_set("Asia/Kolkata");
         $date = date('Y-m-d H:i:s');
@@ -24,8 +24,8 @@
 
         $code = generateRandomString();
 
-        $or_date = date_create($_POST["date"]);
-        $or_date = date_format($or_date, "Y-m-d");
+        $ex_date = date_create($_POST['expire_date_time']);
+        $ex_date = date_format($ex_date, "Y-m-d H:i:s");
 
         foreach($_POST['source'] as $sources)
         {
@@ -57,11 +57,11 @@
             }
         }
 
-        $sql = "insert into cust_order (or_uni_code, or_product, or_price_unit, or_qunatity, or_truck_preference, or_expected_price, or_payment_mode, or_advance_pay, or_expire_on, 
-                or_contact_person_name, or_contact_person_phone) values ('$code', '".$_POST['material']."', '".$_POST['price_unit']."', '".$_POST['quantity']."', 
-                '".$_POST['truck_preference']."', '".$_POST['expected_price']."', '".$_POST['payment_mode']."', '".$_POST['advance_pay']."', '".$_POST['expire_date_time']."', 
-                '".$_POST['contact_person_name']."', '".$_POST['contact_person_phone']."',)";
-        $set = mysql_query($link, $sql);
+        $sql = "insert into cust_order (or_cust_id, or_uni_code, or_product, or_price_unit, or_quantity, or_truck_preference, or_expected_price, or_payment_mode, or_advance_pay, 
+                or_expire_on, or_contact_person_name, or_contact_person_phone) values ('".$_POST['cust_id']."', '$code', '".$_POST['material']."', '".$_POST['price_unit']."', 
+                '".$_POST['quantity']."', '".$_POST['truck_preference']."', '".$_POST['expected_price']."', '".$_POST['payment_mode']."', '".$_POST['advance_pay']."', '$ex_date', 
+                '".$_POST['contact_person_name']."', '".$_POST['contact_person_phone']."')";
+        $set = mysqli_query($link, $sql);
         
         if($set)
         {
@@ -75,5 +75,12 @@
             echo json_encode($responseData, JSON_PRETTY_PRINT);
             http_response_code(400);
         }
+    }
+    else
+    {
+        $responseData = ['success' => '0', 'message' => 'Something went wrong'];
+        echo json_encode($responseData, JSON_PRETTY_PRINT);
+
+        http_response_code(400);
     }
 ?>
