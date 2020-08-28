fb0ada9e2bd8564a05d2dec1f5728a3640f47b13
unknown <rohit1995sgh@gmail.com>
2020-08-28 15:52:44 +0530

    adding Loads to layout

--- layout.php
+++ layout.php
@@ -56,11 +56,12 @@
                 <ul class="sidebar-menu">
                     <li class="menu-header" style="margin-bottom: 50px"></li>
                     <li class="dashboard"><a class="nav-link" href="dashboard"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>
-                    <li class="material_types"><a class="nav-link" href="material_types"><i class="fas fa-truck-loading"></i><span>Material Types</span></a></li>
-                    <li class="truck_cat"><a class="nav-link" href="truck_cat"><i class="fas fa-list"></i><span>Truck Categories</span></a></li>
+                    <li class="loads"><a class="nav-link" href="loads"><i class="fas fa-newspaper"></i><span>Loads</span></a></li>
+                    <li class="shippers"><a class="nav-link" href="shippers"><i class="fas fa-users"></i><span>Shippers</span></a></li>
                     <li class="truck_owners"><a class="nav-link" href="truck_owners"><i class="fas fa-crown"></i><span>Truck Owners</span></a></li>
                     <li class="trucks"><a class="nav-link" href="trucks"><i class="fas fa-truck-monster"></i><span>Trucks</span></a></li>
-                    <li class="shippers"><a class="nav-link" href="shippers"><i class="fas fa-users"></i><span>Shippers</span></a></li>
+                    <li class="truck_cat"><a class="nav-link" href="truck_cat"><i class="fas fa-list"></i><span>Truck Categories</span></a></li>
+                    <li class="material_types"><a class="nav-link" href="material_types"><i class="fas fa-truck-loading"></i><span>Material Types</span></a></li>
                 </ul>
             </aside>
             
