bac12888ac0c5b0cb02108d9fb86c62617cd6978
unknown <rohit1995sgh@gmail.com>
2020-09-08 23:17:05 +0530

    subscription payment date time bug fixed

--- api/subscription_payment.php
+++ api/subscription_payment.php
@@ -6,9 +6,9 @@
     date_default_timezone_set("Asia/Kolkata");
 
     if(isset($_POST['user_type']) && isset($_POST['user_id']) && isset($_POST['amount']) && isset($_POST['duration']) && isset($_POST['razorpay_order_id']) && 
-        isset($_POST['razorpay_payment_id']) && isset($_POST['razorpay_signature']) && isset($_POST['payment_datetime']))
+        isset($_POST['razorpay_payment_id']) && isset($_POST['razorpay_signature']))
     {
-        $date = date_format(date_create($_POST['payment_datetime']),"Y-m-d H:i:s");
+        $date = date("Y-m-d H:i:s");
 
         $days = $_POST['duration'] * 30.4167;
 
@@ -16,14 +16,15 @@
 
         $sql = "insert into subscribed_users (subs_user_type, subs_user_id, subs_amount, subs_duration, razorpay_order_id, razorpay_payment_id, razorpay_signature, 
                 payment_datetime, expire_datetime) values ('".$_POST['user_type']."', '".$_POST['user_id']."', '".$_POST['amount']."', '".$_POST['duration']."', 
-                '".$_POST['razorpay_order_id']."', '".$_POST['razorpay_payment_id']."', '".$_POST['razorpay_signature']."', '".$_POST['payment_datetime']."', '$expire_date')";
+                '".$_POST['razorpay_order_id']."', '".$_POST['razorpay_payment_id']."', '".$_POST['razorpay_signature']."', '$date', '$expire_date')";
         $run = mysqli_query($link, $sql);
 
         if($run)
         {
             if($_POST['user_type'] == 1)
             {
-                $shipper = "update customers set cu_account_on = 2, cu_subscription_order_id = '".$_POST['razorpay_order_id']."', cu_subscription_expire_date = '$expire_date' 
+                $shipper = "update customers set cu_account_on = 2, cu_subscription_start_date = '$date', cu_subscription_order_id = '".$_POST['razorpay_order_id']."', 
+                            cu_subscription_expire_date = '$expire_date' 
                             where cu_id = '".$_POST['user_id']."'";
                 $run_ship = mysqli_query($link, $shipper);
 
