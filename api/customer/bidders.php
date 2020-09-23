8acdccd836bc2b3f658edf1725b74005cb25933f
unknown <rohit1995sgh@gmail.com>
2020-09-23 23:31:40 +0530

    bug fixed

--- api/customer/bidders.php
+++ api/customer/bidders.php
@@ -5,51 +5,109 @@
 
     if(isset($_POST['load_id']))
     {
-        $sql = "select * from bidding where load_id = '".$_POST['load_id']."' and bid_status = 1";
+        $sql = "select * from bidding where load_id = '".$_POST['load_id']."'";
         $run = mysqli_query($link, $sql);
         $counte = mysqli_num_rows($run);
         if($counte >= 1)
         {
             while($row = mysqli_fetch_array($run, MYSQLI_ASSOC))
             {
-                $bidder_detials = array();
-                
-                if($row['bid_user_type'] == 1)
+                if($row['bid_status'] == 1)
                 {
-                    $bidder_type = "Truck Owner";
-                    $bidder_id = $row['bid_user_id'];
+                    $bidder_detials = array();
+                
+                    if($row['bid_user_type'] == 1)
+                    {
+                        $bidder_type = "Truck Owner";
+                        $bidder_id = $row['bid_user_id'];
 
-                    $owner = "select * from truck_owners where to_id = '$bidder_id'";
-                    $get_owner = mysqli_query($link, $owner);
-                    $row_owner = mysqli_fetch_array($get_owner, MYSQLI_ASSOC);
-                    
-                    $bidder_detials[] = ['bidder id' => $row_owner['to_id'], 'bidder name' => $row_owner['to_name'], 
-                                        'bidder phone' => '+'.$row_owner['to_phone_code'].' '.$row_owner['to_phone']];
+                        $owner = "select * from truck_owners where to_id = '$bidder_id'";
+                        $get_owner = mysqli_query($link, $owner);
+                        $row_owner = mysqli_fetch_array($get_owner, MYSQLI_ASSOC);
+                        
+                        $bidder_detials[] = ['bidder id' => $row_owner['to_id'], 'bidder name' => $row_owner['to_name'], 
+                                            'bidder phone' => '+'.$row_owner['to_phone_code'].' '.$row_owner['to_phone']];
+                    }
+                    else
+                    {
+                        $bidder_type = "Driver";
+
+                        $bidder_id = $row['bid_user_id'];
+
+                        $driver = "select * from trucks where trk_id = '$bidder_id'";
+                        $get_driver = mysqli_query($link, $driver);
+                        $row_driver = mysqli_fetch_array($get_driver, MYSQLI_ASSOC);
+                        
+                        $bidder_detials[] = ['bidder id' => $row_driver['trk_id'], 'bidder name' => $row_driver['trk_dr_name'], 
+                                            'bidder phone' => '+'.$row_driver['trk_dr_phone_code'].' '.$row_driver['trk_dr_phone']];
+                    }
+
+                    $loads = "select * from cust_order where or_id = '".$_POST['load_id']."'";
+                    $get_loads = mysqli_query($link, $loads);
+                    $row_loads = mysqli_fetch_array($get_loads, MYSQLI_ASSOC);
+
+                    if($row_loads['or_shipper_on'] == 1)
+                    {
+                        $new_price = $row['bid_expected_price'] + ($row['bid_expected_price'] * ($row_loads['or_admin_expected_price']/100));
+                    }
+                    else
+                    {
+                        $new_price = $row['bid_expected_price'];
+                    }
+
+                    $responseData[] = ['bid id' => $row['bid_id'], 'bidder price' => "$new_price", 'bid status' => '1', 'bidder type' => $bidder_type, 
+                    'bidder details' => $bidder_detials];
+                }
+                elseif($row['bid_status'] == 2)
+                {
+                    $bidder_detials = array();
+                
+                    if($row['bid_user_type'] == 1)
+                    {
+                        $bidder_type = "Truck Owner";
+                        $bidder_id = $row['bid_user_id'];
+
+                        $owner = "select * from truck_owners where to_id = '$bidder_id'";
+                        $get_owner = mysqli_query($link, $owner);
+                        $row_owner = mysqli_fetch_array($get_owner, MYSQLI_ASSOC);
+                        
+                        $bidder_detials[] = ['bidder id' => $row_owner['to_id'], 'bidder name' => $row_owner['to_name'], 
+                                            'bidder phone' => '+'.$row_owner['to_phone_code'].' '.$row_owner['to_phone']];
+                    }
+                    else
+                    {
+                        $bidder_type = "Driver";
+
+                        $bidder_id = $row['bid_user_id'];
+
+                        $driver = "select * from trucks where trk_id = '$bidder_id'";
+                        $get_driver = mysqli_query($link, $driver);
+                        $row_driver = mysqli_fetch_array($get_driver, MYSQLI_ASSOC);
+                        
+                        $bidder_detials[] = ['bidder id' => $row_driver['trk_id'], 'bidder name' => $row_driver['trk_dr_name'], 
+                                            'bidder phone' => '+'.$row_driver['trk_dr_phone_code'].' '.$row_driver['trk_dr_phone']];
+                    }
+
+                    $loads = "select * from cust_order where or_id = '".$_POST['load_id']."'";
+                    $get_loads = mysqli_query($link, $loads);
+                    $row_loads = mysqli_fetch_array($get_loads, MYSQLI_ASSOC);
+
+                    if($row_loads['or_shipper_on'] == 1)
+                    {
+                        $new_price = $row['bid_expected_price'] + ($row['bid_expected_price'] * ($row_loads['or_admin_expected_price']/100));
+                    }
+                    else
+                    {
+                        $new_price = $row['bid_expected_price'];
+                    }
+
+                    $responseData[] = ['bid id' => $row['bid_id'], 'bidder price' => "$new_price", 'bid status' => '2', 'bidder type' => $bidder_type, 
+                    'bidder details' => $bidder_detials];
                 }
                 else
                 {
-                    $bidder_type = "Driver";
 
-                    $bidder_id = $row['bid_user_id'];
-
-                    $driver = "select * from trucks where trk_id = '$bidder_id'";
-                    $get_driver = mysqli_query($link, $driver);
-                    $row_driver = mysqli_fetch_array($get_driver, MYSQLI_ASSOC);
-                    
-                    $bidder_detials[] = ['bidder id' => $row_driver['trk_id'], 'bidder name' => $row_driver['trk_dr_name'], 
-                                        'bidder phone' => '+'.$row_driver['trk_dr_phone_code'].' '.$row_driver['trk_dr_phone']];
                 }
-
-                $loads = "select * from cust_order where or_id = '".$_POST['load_id']."'";
-                $get_loads = mysqli_query($link, $loads);
-                $row_loads = mysqli_fetch_array($get_loads, MYSQLI_ASSOC);
-
-                if($row_loads['or_shipper_on'] == 1)
-                {
-                    $new_price = $row['bid_expected_price'] + ($row['bid_expected_price'] * ($row_loads['or_admin_expected_price']/100));
-                }
-
-                $responseData[] = ['bid id' => $row['bid_id'], 'bidder price' => "$new_price", 'bidder type' => $bidder_type, 'bidder details' => $bidder_detials];
             }
             echo json_encode($responseData, JSON_PRETTY_PRINT);
             http_response_code(200);
