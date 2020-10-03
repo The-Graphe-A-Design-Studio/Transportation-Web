01ae0cb24593931c362985701ae85b8ddb56589f
unknown <rohit1995sgh@gmail.com>
2020-10-03 18:02:49 +0530

    order statistics dashboard

--- dashboard.php
+++ dashboard.php
@@ -24,92 +24,94 @@
                 <div class="row">
                     <div class="col-lg-4 col-md-4 col-sm-12">
                         <div class="card card-statistic-2">
-                            <div class="card-stats">
-                            <div class="card-stats-title">Order Statistics -
-                                <div class="dropdown d-inline">
-                                <a class="font-weight-600 dropdown-toggle" data-toggle="dropdown" href="#" id="orders-month">August</a>
-                                <ul class="dropdown-menu dropdown-menu-sm">
-                                    <li class="dropdown-title">Select Month</li>
-                                    <li><a href="#" class="dropdown-item">January</a></li>
-                                    <li><a href="#" class="dropdown-item">February</a></li>
-                                    <li><a href="#" class="dropdown-item">March</a></li>
-                                    <li><a href="#" class="dropdown-item">April</a></li>
-                                    <li><a href="#" class="dropdown-item">May</a></li>
-                                    <li><a href="#" class="dropdown-item">June</a></li>
-                                    <li><a href="#" class="dropdown-item">July</a></li>
-                                    <li><a href="#" class="dropdown-item active">August</a></li>
-                                    <li><a href="#" class="dropdown-item">September</a></li>
-                                    <li><a href="#" class="dropdown-item">October</a></li>
-                                    <li><a href="#" class="dropdown-item">November</a></li>
-                                    <li><a href="#" class="dropdown-item">December</a></li>
-                                </ul>
+                            <div class="card-stats" style="margin-bottom: 0 !important">
+                                <div class="card-stats-title">
+                                    Order Statistics -
+                                    <div class="dropdown d-inline">
+                                        <select name="" id="" class="common_selector month" style="border: none; text-align: center;">
+                                            <option value="">Month</option>
+                                            <option value="01">January</option>
+                                            <option value="02">February</option>
+                                            <option value="03">March</option>
+                                            <option value="04">April</option>
+                                            <option value="05">May</option>
+                                            <option value="06">June</option>
+                                            <option value="07">July</option>
+                                            <option value="08">August</option>
+                                            <option value="09">September</option>
+                                            <option value="10">October</option>
+                                            <option value="11">November</option>
+                                            <option value="12">December</option>
+                                        </select>
+                                    </div>
+                                    <div class="dropdown d-inline">
+                                        <select name="" id="" class="common_selector year" style="border: none; text-align: center;">
+                                            <option value="<?php echo date('Y'); ?>"><?php echo date('Y'); ?></option>
+                                            <option value="<?php echo date('Y') - 4; ?>"><?php echo date('Y') - 4; ?></option>
+                                            <option value="<?php echo date('Y') - 3; ?>"><?php echo date('Y') - 3; ?></option>
+                                            <option value="<?php echo date('Y') - 2; ?>"><?php echo date('Y') - 2; ?></option>
+                                            <option value="<?php echo date('Y') - 1; ?>"><?php echo date('Y') - 1; ?></option>
+                                            <option value="<?php echo date('Y') + 1; ?>"><?php echo date('Y') + 1; ?></option>
+                                            <option value="<?php echo date('Y') + 2; ?>"><?php echo date('Y') + 2; ?></option>
+                                            <option value="<?php echo date('Y') + 3; ?>"><?php echo date('Y') + 3; ?></option>
+                                            <option value="<?php echo date('Y') + 4; ?>"><?php echo date('Y') + 4; ?></option>
+                                        </select>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="card-stats-items">
-                                <div class="card-stats-item">
-                                <div class="card-stats-item-count">24</div>
-                                <div class="card-stats-item-label">Pending</div>
+                                <div class="card-stats-items filter_data">
+                                    
                                 </div>
-                                <div class="card-stats-item">
-                                <div class="card-stats-item-count">12</div>
-                                <div class="card-stats-item-label">Shipping</div>
-                                </div>
-                                <div class="card-stats-item">
-                                <div class="card-stats-item-count">23</div>
-                                <div class="card-stats-item-label">Completed</div>
-                                </div>
-                            </div>
                             </div>
                             <div class="card-icon shadow-primary bg-primary">
-                            <i class="fas fa-archive"></i>
+                                <i class="fas fa-archive"></i>
                             </div>
                             <div class="card-wrap">
-                            <div class="card-header">
-                                <h4>Total Orders</h4>
-                            </div>
-                            <div class="card-body">
-                                59
-                            </div>
+                                <div class="card-header">
+                                    <h4>Total Loads</h4>
+                                </div>
+                                <div class="card-body total">
+                                    
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-12">
                         <div class="card card-statistic-2">
                             <div class="card-chart">
-                            <canvas id="balance-chart" height="80"></canvas>
+                                <canvas id="balance-chart" height="80"></canvas>
                             </div>
                             <div class="card-icon shadow-primary bg-primary">
-                            <i class="fas fa-dollar-sign"></i>
+                                <i class="fas fa-dollar-sign"></i>
                             </div>
                             <div class="card-wrap">
-                            <div class="card-header">
-                                <h4>Balance</h4>
-                            </div>
-                            <div class="card-body">
-                                $187,13
-                            </div>
+                                <div class="card-header">
+                                    <h4>Balance</h4>
+                                </div>
+                                <div class="card-body">
+                                    $187,13
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-12">
                         <div class="card card-statistic-2">
                             <div class="card-chart">
-                            <canvas id="sales-chart" height="80"></canvas>
+                                <canvas id="sales-chart" height="80"></canvas>
                             </div>
                             <div class="card-icon shadow-primary bg-primary">
-                            <i class="fas fa-shopping-bag"></i>
+                                <i class="fas fa-shopping-bag"></i>
                             </div>
                             <div class="card-wrap">
-                            <div class="card-header">
-                                <h4>Sales</h4>
-                            </div>
-                            <div class="card-body">
-                                4,732
-                            </div>
+                                <div class="card-header">
+                                    <h4>Sales</h4>
+                                </div>
+                                <div class="card-body">
+                                    4,732
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </div>                
             </section>
         </div>
 
@@ -118,9 +120,51 @@
     </div>
 
     <?php echo $script_tags; ?>
+
     <script type="text/javascript">
-        $(document).ready(function(){
+        $(document).ready(function()
+        {
+            filter_data();
+        
+            function filter_data()
+            {
+                var action = 'fetch_data';
+                var month = months();
+                var year = years();
+                $.ajax({
+                    url:"processing/curd_dashboard.php",
+                    method:"POST",
+                    data:{action:action, year:year, month:month},
+                    success:function(data){
+                        $('.filter_data').html(data);
+                        var hold = $("#hold").text();
+                        var going = $("#going").text();
+                        var complete = $("#complete").text();
+                        var total = parseInt(hold) + parseInt(going) + parseInt(complete);
+                        $('.total').html(total);
+                    }
+                });
+            }
+
+            function months()
+            {
+                return $('.month').find('option:selected').val();
+            }
+
+            function years()
+            {
+                return $('.year').find('option:selected').val();
+            }
+            
+            $('.common_selector').on('keyup change',function(){
+                filter_data();
+                months();
+                years()
+            });
+            
             $(".dashboard").addClass("active");
+
+            
         });
     </script>
 </body>
---
+++ processing/curd_dashboard.php
@@ -0,0 +1,93 @@
+<?php
+
+    include('../session.php');
+
+    $connect = new PDO("mysql:host=$hostName; dbname=$dbName", $userName, $password);
+
+    if(isset($_POST["action"]))
+    {
+        $query = "select * from cust_order where or_default = 1";
+
+        $month = $year = '';
+
+        if(!empty($_POST["month"]))
+        {
+            $query .= " and DATE_FORMAT(or_active_on, '%m') = '".$_POST["month"]."'";
+            $month = " and DATE_FORMAT(or_active_on, '%m') = '".$_POST["month"]."'";
+        }
+
+        if(!empty($_POST["year"]))
+        {
+            $query .= " and DATE_FORMAT(or_active_on, '%Y') = '".$_POST["year"]."'";
+            $year = " and DATE_FORMAT(or_active_on, '%Y') = '".$_POST["year"]."'";
+        }
+
+        // echo $query;
+
+        $statement = $connect->prepare($query);
+        $statement->execute();
+        $result = $statement->fetchAll();
+        $total_row = $statement->rowCount();
+
+        // echo $total_row; 
+
+        $output = '';
+        
+        if($total_row > 0)
+        {
+            $hold = "select * from cust_order where or_status = 2 ".$month.$year;
+            $run_hold = mysqli_query($link, $hold);
+            $count_hold = mysqli_num_rows($run_hold);
+
+            $going = "select * from cust_order where or_status = 4 ".$month.$year;
+            $run_going = mysqli_query($link, $going);
+            $count_going = mysqli_num_rows($run_going);
+
+            $complete = "select * from cust_order where or_status = 5 ".$month.$year;
+            $run_complete = mysqli_query($link, $complete);
+            $count_complete = mysqli_num_rows($run_complete);
+
+            $output .=
+            '
+                <div class="card-stats-item">
+                    <div class="card-stats-item-count" id="hold">'.$count_hold.'</div>
+                    <div class="card-stats-item-label">On Hold</div>
+                </div>
+                <div class="card-stats-item">
+                    <div class="card-stats-item-count" id="going">'.$count_going.'</div>
+                    <div class="card-stats-item-label">On Going</div>
+                </div>
+                <div class="card-stats-item">
+                    <div class="card-stats-item-count" id="complete">'.$count_complete.'</div>
+                    <div class="card-stats-item-label">Completed</div>
+                </div>
+            ';
+        }
+        else
+        {
+            $output = 
+            '
+                <div class="card-stats-item">
+                    <div class="card-stats-item-count" id="hold">0</div>
+                    <div class="card-stats-item-label">On Hold</div>
+                </div>
+                <div class="card-stats-item">
+                    <div class="card-stats-item-count" id="going">0</div>
+                    <div class="card-stats-item-label">On Going</div>
+                </div>
+                <div class="card-stats-item">
+                    <div class="card-stats-item-count" id="complete">0</div>
+                    <div class="card-stats-item-label">Completed</div>
+                </div>
+            ';
+        }
+        
+        echo $output;
+
+    }
+    else
+    {
+        echo "Server error";
+    }
+
+?>
