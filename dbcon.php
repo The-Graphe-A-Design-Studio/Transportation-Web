3445feefc8ea5899a7d00969c3c3eb351b505df5
unknown <rohit1995sgh@gmail.com>
2020-10-04 21:08:08 +0530

    Check and delete old bids if load is set to completed

--- dbcon.php
+++ dbcon.php
@@ -104,4 +104,13 @@
     {
         mysqli_query($link, "update cust_order set or_status = 5 where or_id = '".$row11['or_id']."'");
     }
+
+
+    // Checking for old bids if load is set to completed
+    $sql5 = "select * from cust_order where or_status = 5";
+    $run5 = mysqli_query($link, $sql5);
+    while($row5 = mysqli_fetch_array($run5, MYSQLI_ASSOC))
+    {
+        mysqli_query($link, "delete from bidding where load_id = '".$row5['or_id']."'");
+    }
 ?>
