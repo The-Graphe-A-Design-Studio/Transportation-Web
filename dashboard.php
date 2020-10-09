b285746d9f3a04a3554a0b555adaa641e82333a4
unknown <rohit1995sgh@gmail.com>
2020-10-09 14:19:50 +0530

    options 2020 to 2030

--- dashboard.php
+++ dashboard.php
@@ -46,15 +46,18 @@
                                     </div>
                                     <div class="dropdown d-inline">
                                         <select class="common_selector year" style="border: none; text-align: center;">
-                                            <option value="<?php echo date('Y'); ?>"><?php echo date('Y'); ?></option>                                            
-                                            <option value="<?php echo date('Y') - 4; ?>"><?php echo date('Y') - 4; ?></option>
-                                            <option value="<?php echo date('Y') - 3; ?>"><?php echo date('Y') - 3; ?></option>
-                                            <option value="<?php echo date('Y') - 2; ?>"><?php echo date('Y') - 2; ?></option>
-                                            <option value="<?php echo date('Y') - 1; ?>"><?php echo date('Y') - 1; ?></option>
-                                            <option value="<?php echo date('Y') + 1; ?>"><?php echo date('Y') + 1; ?></option>
-                                            <option value="<?php echo date('Y') + 2; ?>"><?php echo date('Y') + 2; ?></option>
-                                            <option value="<?php echo date('Y') + 3; ?>"><?php echo date('Y') + 3; ?></option>
-                                            <option value="<?php echo date('Y') + 4; ?>"><?php echo date('Y') + 4; ?></option>
+                                            <option value="">Year</option>                                            
+                                            <option value="2020">2020</option>
+                                            <option value="2021">2021</option>
+                                            <option value="2022">2022</option>
+                                            <option value="2023">2023</option>
+                                            <option value="2024">2024</option>
+                                            <option value="2025">2025</option>
+                                            <option value="2026">2026</option>
+                                            <option value="2027">2027</option>
+                                            <option value="2028">2028</option>
+                                            <option value="2029">2029</option>
+                                            <option value="2030">2030</option>
                                         </select>
                                     </div>
                                 </div>
