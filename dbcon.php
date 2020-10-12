3d509198abf5fd3e5beb60e187ae28ea0e9f9f2f
unknown <rohit1995sgh@gmail.com>
2020-10-12 14:46:02 +0530

    bug fixed

--- dbcon.php
+++ dbcon.php
@@ -101,12 +101,13 @@
     $count11 = mysqli_num_rows($run11);
     if($count11 != 0)
     {
+        $row11 = mysqli_fetch_array($run11, MYSQLI_ASSOC);
+
         if($row11['del_status'] == 2)
         {
             mysqli_query($link, "update cust_order set or_status = 5 where or_id = '".$row11['or_id']."'");
         }
-
-
+        
         // Checking for old bids if load is set to completed
         $sql5 = "select * from cust_order where or_status = 5";
         $run5 = mysqli_query($link, $sql5);
