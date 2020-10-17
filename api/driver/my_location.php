5ed2543de3cdbd5b2d46d238a60eaa22644bee9e
unknown <rohit1995sgh@gmail.com>
2020-10-17 18:37:50 +0530

    driver location api updated

--- api/driver/my_location.php
+++ api/driver/my_location.php
@@ -3,23 +3,46 @@
 
     header('Content-Type: application/json');
 
-    if(isset($_POST['del_trk_id']) && isset($_POST['lat']) && isset($_POST['lng']))
+    if(isset($_POST['truck_id']) && isset($_POST['del_trk_id']) && isset($_POST['lat']) && isset($_POST['lng']))
     {
-        $active = "update delivery_trucks set lat = '".$_POST['lat']."', lng = '".$_POST['lng']."' where del_trk_id = '".$_POST['del_trk_id']."'";
-        $set = mysqli_query($link, $active);
-
-        if($set)
+        if(empty($_POST['del_trk_id']))
         {
-            $responseData = ['success' => '1', 'message' => 'Location updated'];
-            echo json_encode($responseData, JSON_PRETTY_PRINT);
-            http_response_code(200);
+            $location = "update trucks set trk_lat = '".$_POST['lat']."', trk_lng = '".$_POST['lng']."' where trk_id = '".$_POST['truck_id']."'";
+            $run_loc = mysqli_query($link, $location);
+
+            if($run_loc)
+            {
+                $responseData = ['success' => '1', 'message' => 'Location updated'];
+                echo json_encode($responseData, JSON_PRETTY_PRINT);
+                http_response_code(200);
+            }
+            else
+            { 
+                $responseData = ['success' => '0', 'message' => 'Location update failed'];
+                echo json_encode($responseData, JSON_PRETTY_PRINT);
+                http_response_code(400);
+            }
         }
         else
-        { 
-            $responseData = ['success' => '0', 'message' => 'Location update failed'];
-            echo json_encode($responseData, JSON_PRETTY_PRINT);
-            http_response_code(400);
-        }
+        {
+            $active = "update delivery_trucks set lat = '".$_POST['lat']."', lng = '".$_POST['lng']."' where del_trk_id = '".$_POST['del_trk_id']."' and trk_id = '".$_POST['truck_id']."'";
+            $set = mysqli_query($link, $active);
+
+            if($set)
+            {
+                mysqli_query($link, "update trucks set trk_lat = '".$_POST['lat']."', trk_lng = '".$_POST['lng']."' where trk_id = '".$_POST['truck_id']."'");
+
+                $responseData = ['success' => '1', 'message' => 'Location updated'];
+                echo json_encode($responseData, JSON_PRETTY_PRINT);
+                http_response_code(200);
+            }
+            else
+            { 
+                $responseData = ['success' => '0', 'message' => 'Location update failed'];
+                echo json_encode($responseData, JSON_PRETTY_PRINT);
+                http_response_code(400);
+            }
+        }        
     }
     else
     {
