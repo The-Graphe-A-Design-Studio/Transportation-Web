1669125618ebea5755ec3796bf90f2fb3a6d5081
unknown <rohit1995sgh@gmail.com>
2020-10-20 18:01:21 +0530

    bug fixed

--- api/add_on_truck_plan.php
+++ api/add_on_truck_plan.php
@@ -10,21 +10,12 @@
     $emparray = array();
     while($row =mysqli_fetch_assoc($result))
     {
-        if($row['plan_duration'] > 1)
-        {
-            $truck = $row['plan_duration'].' Trucks';
-        }
-        else
-        {
-            $truck = $row['plan_duration'].' Truck';
-        }
-
         $total_price = round(($row['plan_selling_price'] + ($row['plan_selling_price'] * 0.18)), 2);
 
         $discount = ( ( ( $row['plan_original_price'] - $row['plan_selling_price'] ) / $row['plan_original_price'] ) * 100 );
 
         $emparray[] = ['plan id' => $row['plan_id'], 'plan name' => $row['plan_name'], 'plan type' => $row['plan_type'], 'plan original price' => $row['plan_original_price'], 'plan selling price' => $row['plan_selling_price'], 
-                        'plan discount' => round($discount, 2).'%', 'GST' => '18%', 'final price' => "$total_price", 'quantity' => $trucks];
+                        'plan discount' => round($discount, 2).'%', 'GST' => '18%', 'final price' => "$total_price", 'quantity' => $row['plan_duration']];
     }
     header('Content-Type: application/json');
     echo json_encode($emparray, JSON_PRETTY_PRINT);
