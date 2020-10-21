a498285652e3a6faa1b66bd90702dd2be2b502eb
unknown <rohit1995sgh@gmail.com>
2020-10-21 20:47:27 +0530

    new shipper registered, admin gets notification

--- api/customer/shipper_enter_exit.php
+++ api/customer/shipper_enter_exit.php
@@ -46,6 +46,11 @@
                 $response = curl_exec($curl);
 
                 curl_close($curl);
+
+                $no_title = "New Shipper Registered";
+                $no_message = "New shipper registered with phone number ".$_POST['cu_phone'];
+                $no_for_id = $_POST['cu_phone'];
+                mysqli_query($link, "insert into notifications (no_title, no_message, id) values('$no_title', '$no_message', '$no_for_id')");
                 
                 $responseData = ['success' => '1', 'message' => 'OTP sent to your given number. Please verify your number'];
                 echo json_encode($responseData, JSON_PRETTY_PRINT);
