e9307e96dfc7a03a65b052f1391fe215d16f2dcf
unknown <rohit1995sgh@gmail.com>
2020-11-05 19:51:22 +0530

    truck owner my delliveries api updated

--- api/truck_owner/my_deliveries.php
+++ api/truck_owner/my_deliveries.php
@@ -169,7 +169,16 @@
                         $run_t_info = mysqli_query($link, $t_info);
                         $row_t_info = mysqli_fetch_array($run_t_info, MYSQLI_ASSOC);
 
-                        $del_trucks[] = ['del truck id' => $row_del_t['del_trk_id'], 'truck number' => $row_t_info['trk_num'], 'driver name' => $row_t_info['trk_dr_name'], 'driver phone' => $row_t_info['trk_dr_phone']];
+                        if($row_del_t['otp_verified'] == 1)
+                        {
+                            $del_trucks[] = ['del truck id' => $row_del_t['del_trk_id'], 'truck number' => $row_t_info['trk_num'], 'driver name' => $row_t_info['trk_dr_name'], 
+                                        'driver phone' => $row_t_info['trk_dr_phone'], 'latitude' => $row_del_t['lat'], 'longitude' => $row_del_t['lng'], 'status' => $row_del_t['status']];
+                        }
+                        else
+                        {
+                            $del_trucks[] = ['del truck id' => $row_del_t['del_trk_id'], 'truck number' => $row_t_info['trk_num'], 'driver name' => $row_t_info['trk_dr_name'], 
+                                        'driver phone' => $row_t_info['trk_dr_phone'], 'latitude' => $row_del_t['lat'], 'longitude' => $row_del_t['lng'], 'otp' => $row_del_t['otp']];
+                        }
                     }
 
                     $delivery_trucks = ['status' => '1', 'trucks' => $del_trucks];
