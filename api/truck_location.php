159c76512e8879ba6ce4f5075d432a72e3f9b8a3
unknown <rohit1995sgh@gmail.com>
2020-11-05 21:12:56 +0530

    delivery truck location api

--- api/truck_location.php
+++ api/truck_location.php
@@ -14,6 +14,17 @@
 
         http_response_code(200);
     }
+    if(isset($_POST['delivery_truck_id']))
+    {
+        $sql = "select * from delivery_trucks where del_trk_id = '".$_POST['delivery_truck_id']."'";
+        $run = mysqli_query($link, $sql);
+        $row = mysqli_fetch_array($run, MYSQLI_ASSOC);
+
+        $responseData = ['delivery truck id' => $row['del_trk_id'], 'lat' => $row['lat'], 'lng' => $row['lng']];
+        echo json_encode($responseData, JSON_PRETTY_PRINT);
+
+        http_response_code(200);
+    }
     else
     {
         $responseData = ['success' => '0', 'message' => 'Something is missing'];
