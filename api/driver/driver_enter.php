d85408e3712e286026b01144c5fd67b0ff16e240
unknown <rohit1995sgh@gmail.com>
2021-06-09 15:09:03 +0530

    dummy login for driver

--- api/driver/driver_enter.php
+++ api/driver/driver_enter.php
@@ -14,60 +14,70 @@
         $re_r = mysqli_query($link, $re_c);
         $row_c = mysqli_fetch_array($re_r, MYSQLI_ASSOC);
         $counte = mysqli_num_rows($re_r);
-        if($counte == 1)
+
+        if($phone == 9999988888)
         {
-            $rantrk_no = rand(100000, 999999);
-
-            $mobile_sql = "update trucks set trk_otp = '$rantrk_no', trk_dr_token = '$token' where trk_dr_phone = '$phone'";
-
-            $mobile_insert = mysqli_query($link, $mobile_sql);
-            
-            if($mobile_insert)
+            $responseData = ['success' => '1', 'message' => 'Your OTP is 123456.'];
+            echo json_encode($responseData, JSON_PRETTY_PRINT);
+            http_response_code(200);
+        }
+        else
+        {
+            if($counte == 1)
             {
-                $api = '321698AFaajkjGny5f65b2b7P1';
-                $template = '5f995d9b6ddb9b27a7274a72';
-                
-                $curl = curl_init();
+                $rantrk_no = rand(100000, 999999);
 
-                curl_setopt_array($curl, array(
-                  CURLOPT_URL => "https://api.msg91.com/api/v5/otp?authkey=$api&template_id=$template&mobile=91$phone&invisible=1&otp=$rantrk_no",
-                  CURLOPT_RETURNTRANSFER => true,
-                  CURLOPT_ENCODING => "",
-                  CURLOPT_MAXREDIRS => 10,
-                  CURLOPT_TIMEOUT => 30,
-                  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                  CURLOPT_CUSTOMREQUEST => "GET",
-                  CURLOPT_SSL_VERIFYHOST => 0,
-                  CURLOPT_SSL_VERIFYPEER => 0,
-                  CURLOPT_HTTPHEADER => array(
-                    "content-type: application/json"
-                  ),
-                ));
-                
-                $response = curl_exec($curl);
-                $err = curl_error($curl);
-                
-                curl_close($curl);
-                
-                $responseData = ['success' => '1', 'message' => 'OTP sent to your given number. Please verify your number'];
-                echo json_encode($responseData, JSON_PRETTY_PRINT);
+                $mobile_sql = "update trucks set trk_otp = '$rantrk_no', trk_dr_token = '$token' where trk_dr_phone = '$phone'";
 
-                http_response_code(200);
-            } 
+                $mobile_insert = mysqli_query($link, $mobile_sql);
+                
+                if($mobile_insert)
+                {
+                    $api = '321698AFaajkjGny5f65b2b7P1';
+                    $template = '5f995d9b6ddb9b27a7274a72';
+                    
+                    $curl = curl_init();
+
+                    curl_setopt_array($curl, array(
+                    CURLOPT_URL => "https://api.msg91.com/api/v5/otp?authkey=$api&template_id=$template&mobile=91$phone&invisible=1&otp=$rantrk_no",
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 30,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "GET",
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                    CURLOPT_HTTPHEADER => array(
+                        "content-type: application/json"
+                    ),
+                    ));
+                    
+                    $response = curl_exec($curl);
+                    $err = curl_error($curl);
+                    
+                    curl_close($curl);
+                    
+                    $responseData = ['success' => '1', 'message' => 'OTP sent to your given number. Please verify your number'];
+                    echo json_encode($responseData, JSON_PRETTY_PRINT);
+
+                    http_response_code(200);
+                } 
+                else
+                {
+                    $responseData = ['success' => '0', 'message' => 'Something went wrong. Error'];
+                    echo json_encode($responseData, JSON_PRETTY_PRINT);
+
+                    http_response_code(400);
+                }
+            }
             else
             {
-                $responseData = ['success' => '0', 'message' => 'Something went wrong. Error'];
+                $responseData = ['success' => '0', 'message' => 'This number is not registered. Contact your truck owner.'];
                 echo json_encode($responseData, JSON_PRETTY_PRINT);
 
                 http_response_code(400);
             }
-        }
-        else
-        {
-            $responseData = ['success' => '0', 'message' => 'This number is not registered. Contact your truck owner.'];
-            echo json_encode($responseData, JSON_PRETTY_PRINT);
-
-            http_response_code(400);
         }
     }
     else
